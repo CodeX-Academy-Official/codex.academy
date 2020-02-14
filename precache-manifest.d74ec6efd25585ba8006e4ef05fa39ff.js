6fb2349699a361220f17e33f165a1f2f977e0825
Byron Sommardahl <byron@sommardahl.com>
2020-02-14 17:14:03 -0600

    Updates

---
+++ precache-manifest.d74ec6efd25585ba8006e4ef05fa39ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5f2c819bf2295e48f5",
+    "url": "/css/app.af1ad4f5.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "628ee7acb4e7be75874b11a0289d04e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1072118afd1353c8e54",
+    "url": "/js/about.ccb1056e.js"
+  },
+  {
+    "revision": "eb5f2c819bf2295e48f5",
+    "url": "/js/app.b04e56b7.js"
+  },
+  {
+    "revision": "78185cbc519ba1e04382",
+    "url": "/js/chunk-vendors.9bbf69e0.js"
+  },
+  {
+    "revision": "db31bf4807f47c668280a84a26a5bfcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
