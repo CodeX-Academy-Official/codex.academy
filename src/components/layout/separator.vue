25eb869c217203ce3ebb00800ed8988c3ac44b3d
RonyVidaur <rony.vidaur@gmail.com>
2020-02-25 17:53:56 -0600

    re applying improvements

---
+++ src/components/layout/separator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="separator"></div>
+</template>
+
+<style scoped>
+  .separator {
+    display: none;
+  }
+
+  @media (max-width: 991.98px) { 
+  .separator {
+    height: 1px;
+    width: 60%;
+    margin-left: 20%;
+    margin-right: auto;
+    background-color: #7b7c7e;
+    margin-top: 5px;
+    display: block;
+  }
+}
+
+</style>
