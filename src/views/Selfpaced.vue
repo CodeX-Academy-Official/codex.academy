ccf2ce0a7cbb208d5b8a241dfd325261e77fdd5c
Christian Morsing <cmorsing@litepoint.internal>
2020-03-09 21:43:43 -0500

    Updated with plans Navigation

--- src/views/Selfpaced.vue
+++ src/views/Selfpaced.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
-    <!-- Page Heading/Breadcrumbs -->
-    <h1 class="mt-4 mb-3">Bootcamps</h1>
+    <h1 class="mt-4 mb-3">Self-Paced Mentoring Plans</h1>
 
     <ol class="breadcrumb">
       <li class="breadcrumb-item">
@@ -9,12 +8,12 @@
       </li>
       <li class="breadcrumb-item">
         <router-link to="/Plans">Plans</router-link></li>
-      <li class="breadcrumb-item active">Bootcamps</li>
+      <li class="breadcrumb-item active">Self-Paced</li>
     </ol>
 
     <div class="row d-none d-lg-block">
       <Unsplash
-        imageId="oqStl2L5oxI"
+        imageId="ieic5Tq8YMk"
         align="right"
         description="Coffee-driven development"
         width="1100"
@@ -25,26 +24,31 @@
 
     <div class="row mt-5">
       <h4 class="text-center">
-        Looking for an intense, immersive learning experience? CodeX Academy has
-        a few bootcamp options to choose from, depending on how far you want to
-        go.
+        At CodeX Academy the WORLD will be a better place if YOU are coding!
+        Because of that, we strive to provide the most flexible options possible
+        so that you have the best chance to learn.
       </h4>
     </div>
-
     <div class="row mt-5">
-      <BootcampsTable />
+      <PlansTable />
     </div>
   </div>
 </template>
 
 <script>
-import BootcampsTable from "@/components/BootcampsTable";
+import PlansTable from "@/components/PlanTable";
 import Unsplash from "@/components/Unsplash";
 
 export default {
   components: {
-    BootcampsTable,
+    PlansTable,
     Unsplash
   }
 };
 </script>
+
+<style scoped>
+.bootcamp-description {
+  height: 2rem;
+}
+</style>
