ad821c0fd24a94e0e2b77a44ddf6330f66c56b3d
Byron Sommardahl <byron@sommardahl.com>
2020-03-20 15:34:00 -0500

    Restyled the testimonials.

--- src/components/TestimonialsCarousel.vue
+++ src/components/TestimonialsCarousel.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="row">
-    <div class="col">
+    <div class="col col-12 col-md-4">
       <testimonial
         name="Randy K."
-        location ="Miami,FL"
+        location="Miami,FL"
         headshot="img/headshots/randy.jpg"
         comments="The CodeX curriculum allowed me to work at my own pace with mentors who were flexible with my ever changing schedule. As a busy professional, the scheduling flexibility made a big difference in helping me successfully complete the curriculum."
       />
     </div>
-    <div class="col">
+    <div class="col col-12 col-md-4">
       <testimonial
         name="Louis P."
-        location ="Nashville,TN"
+        location="Nashville,TN"
         headshot="img/headshots/louis.jpg"
         comments="I’ve wanted to learn code for years. CodeX has mentors that ensure I am understanding the material. I have learned so much and I am set up for success, I love it!"
       />
     </div>
-    <div class="col">
+    <div class="col col-12 col-md-4">
       <testimonial
         name="LeAnne B."
-        location ="Atlanta,GA"
+        location="Atlanta,GA"
         headshot="img/headshots/leanne.jpg"
         comments="CodeX was a really fun experience and has been valuable for my career!"
       />
--- src/components/testimonial.vue
+++ src/components/testimonial.vue
@@ -3,14 +3,31 @@
     <div class="headshot d-flex">
       <img :src="headshot" class="img-fluid mx-auto mb-4" />
     </div>
-    <h1>{{ name }}</h1> 
-    <h5>{{ location }}</h5>
-    <p class="pl-2 pr-2">{{ comments }}</p>
+    <p class="text-center pl-3 pr-3 stars">
+      <font-awesome-icon :icon="star" class="checked" />
+      <font-awesome-icon :icon="star" class="checked ml-1" />
+      <font-awesome-icon :icon="star" class="checked ml-1" />
+      <font-awesome-icon :icon="star" class="checked ml-1" />
+      <font-awesome-icon :icon="star" class="checked ml-1" />
+    </p>
+    <blockquote class="blockquote d-block w-75 mx-auto text-center">
+      {{ comments }}
+      <footer class="blockquote-footer text-right">
+        {{name}}
+        <br />
+        <cite title="Location">{{location}}</cite>
+      </footer>
+    </blockquote>
   </div>
 </template>
 
 <script>
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
 export default {
+  data: () => ({
+    star: faStar
+  }),
   props: {
     headshot: String,
     name: String,
@@ -20,7 +37,23 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-img { border-radius: 50%; width: 60%; }
-p { text-align: left;}
+<style lang="scss" scoped>
+@import "@/variables";
+
+img {
+  border-radius: 50%;
+  width: 60%;
+}
+p {
+  text-align: left;
+}
+.stars {
+  .checked {
+    color: $primary;
+  }
+}
+
+blockquote {
+  font-size: 1rem;
+}
 </style>
