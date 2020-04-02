3d59faf4a5de7d93b447dc113a89ceef5ee25174
Byron Sommardahl <byron@sommardahl.com>
2020-04-02 12:45:26 -0500

    Fixing page bug.

--- src/router/scrollToHash.ts
+++ src/router/scrollToHash.ts
@@ -1,6 +1,7 @@
 export function scrollToHash(hash: string) {
   const parts = hash.split("#");
   const clean = "#" + parts[parts.length - 1];
+  if (clean === "#/") return;
   const selector: any = document.querySelector(clean);
   if (selector) {
     return window.scrollTo({
