d99e146770473a4ef12c2d6b29d678ee5444e38f
Byron Sommardahl <byron@sommardahl.com>
2020-04-02 12:58:56 -0500

    Making fix more robust to cover other scenarios.

--- src/router/scrollToHash.ts
+++ src/router/scrollToHash.ts
@@ -1,7 +1,7 @@
 export function scrollToHash(hash: string) {
   const parts = hash.split("#");
   const clean = "#" + parts[parts.length - 1];
-  if (clean === "#/") return;
+  if (clean.indexOf("/") > -1) return;
   const selector: any = document.querySelector(clean);
   if (selector) {
     return window.scrollTo({
