03cf3e7e99cc11c35e4c8b13d68d3a544f2c0afe
Byron Sommardahl <byron@sommardahl.com>
2020-04-05 13:26:08 -0500

    Refactoring icons and stats.

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,20 +1,45 @@
 <template>
-  <img svg-inline class="icon" :src="src" :alt="name" />
+  <inline-svg
+    :src="src"
+    @loaded="loaded($event)"
+    @error="error($event)"
+    width="100"
+    height="100"
+    fill="black"
+    :aria-label="name"
+  ></inline-svg>
+
+  <!-- <img svg-inline class="icon" :src="src" :alt="name" /> -->
 </template>
 
 <script>
+import InlineSvg from "vue-inline-svg";
+
+// Your component
 export default {
+  components: {
+    InlineSvg,
+  },
   props: {
-    name: { type: String, default: "agile" }
+    name: { type: String, default: "agile" },
   },
   computed: {
     src() {
-      return `img/noun/${this.name.toLowerCase()}.svg`;
-    }
-  }
+      //return `img/noun/${this.name.toLowerCase()}.svg`;
+      //const path = `@/components/svg/noun/${this.name.toLowerCase()}.svg`;
+      const path = "./svg/noun/happy.svg";
+      return require(path);
+    },
+  },
+  methods: {
+    error(ev) {
+      console.error(ev);
+    },
+    loaded(ev) {
+      console.log(ev);
+    },
+  },
 };
 </script>
-
-//Docs: https://www.npmjs.com/package/vue-svg-inline-loader
 
 <style></style>
