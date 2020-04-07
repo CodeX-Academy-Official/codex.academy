7157aa6bbd76ba931b42834f61856809d6cc01c1
Byron Sommardahl <byron@sommardahl.com>
2020-04-07 10:28:55 -0500

    Changing enroll to apply.

--- src/components/PlanCard.vue
+++ src/components/PlanCard.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="h-100">
-    <div
-      class="card"
-      v-bind:class="{ 'card-outline-primary': plan.primary }"
-      v-if="plan"
-    >
+    <div class="card" v-bind:class="{ 'card-outline-primary': plan.primary }" v-if="plan">
       <h3
         class="card-header text-center"
         v-bind:class="{
           'bg-primary': plan.primary,
           'text-white': plan.primary
         }"
-      >
-        {{ plan.title }}
-      </h3>
+      >{{ plan.title }}</h3>
       <div class="text-center mt-4">
         <div class="display-4">${{ price }}</div>
         <div class="font-italic">{{ plan.duration }}</div>
@@ -22,15 +16,11 @@
         <li class="list-group-item" v-if="plan.description">
           <div class="plan-description">{{ plan.description }}</div>
         </li>
-        <li
-          class="list-group-item"
-          v-for="d in plan.details"
-          :key="d.text || d"
-        >
+        <li class="list-group-item" v-for="d in plan.details" :key="d.text || d">
           <div :class="d.css">{{ d.text || d }}</div>
         </li>
         <li class="list-group-item" v-if="selectable">
-          <SelectPlanButton :plan="plan" text="Enroll" />
+          <SelectPlanButton :plan="plan" text="Apply" />
         </li>
         <li class="list-group-item" v-if="selectable">
           <router-link to="/findplan">Help Me Decide</router-link>
