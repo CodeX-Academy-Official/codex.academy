b56480a94b54237a4599bd96de6f3793de6210b5
Byron Sommardahl <byron@sommardahl.com>
2020-04-13 14:32:05 -0500

    Fixing responsive formatting for mobile in footer.

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="container">
       <div class="row justify-content-md-center text-center">
-        <div class="col-6 col-md-2">
+        <div class="col-12 col-md-2 mb-5">
           <h4>CODEX Academy</h4>
           <p>
             <router-link to="/">Home</router-link>
@@ -19,7 +19,7 @@
           </p>
         </div>
 
-        <div class="col-6 col-md-2">
+        <div class="col-12 col-md-2 mb-5">
           <h4>Programs</h4>
           <p>
             <router-link to="/programs/overview">Overview</router-link>
@@ -38,7 +38,7 @@
           </p>
         </div>
 
-        <div class="col-6 col-md-2">
+        <div class="col-12 col-md-2 mb-5">
           <h4>How It Works</h4>
           <p>
             <router-link to="/howitworks#pillars">Six Pillars</router-link>
@@ -54,7 +54,7 @@
           </p>
         </div>
 
-        <div class="col-6 col-md-2">
+        <div class="col-12 col-md-2 mb-5">
           <h4>Help</h4>
           <p>
             <router-link to="/faqs">FAQs</router-link>
@@ -64,7 +64,7 @@
           </p>
         </div>
 
-        <div class="col col-md-2">
+        <div class="col col-md-2 mb-5">
           <h4>Legal</h4>
           <p>
             <router-link to="/policy/privacy">Privacy</router-link>
@@ -78,7 +78,7 @@
     <div>
       <hr />
 
-      <div class="row justify-content-md-center text-center mt-5 mb-4">
+      <div class="row justify-content-center text-center mt-5 mb-4">
         <div class="col-1">
           <a href="https://facebook.com/codexacademyonline" target="_blank">
             <font-awesome-icon :icon="faFacebookSquare" class="fa-2x" />
