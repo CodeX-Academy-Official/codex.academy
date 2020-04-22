47bad201e631f39f220524594f5bcddada2cfae4
Byron Sommardahl <byron@sommardahl.com>
2020-04-22 15:59:11 -0500

    Adding more info to the deferred tuition page.

---
+++ public/img/headshots/CodeX Academy - Learn to code. Change the world._files/app.5f04e5a108138cbd50a6.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("app",{
+
+/***/ "./src/components/svg/noun/coins.svg":
+/*!*******************************************!*\
+  !*** ./src/components/svg/noun/coins.svg ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"img/coins.e87deeba.svg\";//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvY29tcG9uZW50cy9zdmcvbm91bi9jb2lucy5zdmcuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvY29tcG9uZW50cy9zdmcvbm91bi9jb2lucy5zdmc/ZTRiYyJdLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IF9fd2VicGFja19wdWJsaWNfcGF0aF9fICsgXCJpbWcvY29pbnMuZTg3ZGVlYmEuc3ZnXCI7Il0sIm1hcHBpbmdzIjoiQUFBQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/components/svg/noun/coins.svg\n");
+
+/***/ })
+
+})
