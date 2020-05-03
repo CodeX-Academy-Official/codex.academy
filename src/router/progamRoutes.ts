3fefe12b9e4724054d08eab74c4bc00a3d55682d
Byron Sommardahl <byron@sommardahl.com>
2020-05-03 00:35:50 -0500

    Adding front-end dev

--- src/router/progamRoutes.ts
+++ src/router/progamRoutes.ts
@@ -1,5 +1,6 @@
 import Programs from "../views/Programs.vue";
 import FullStackDeveloper from "../views/programs/fullstackdev.vue";
+import FrontEndDeveloper from "../views/programs/frontenddev.vue";
 
 const routes: Array<any> = [
   {
@@ -8,6 +9,6 @@ const routes: Array<any> = [
   },
   { path: "/programs/full-stack-dev", component: FullStackDeveloper },
   // { path: "/programs/full-stack-eng", component: FullStackDeveloper },
-  // { path: "/programs/front-end-dev", component: FullStackDeveloper },
+  { path: "/programs/front-end-dev", component: FrontEndDeveloper },
 ];
 export default routes;
