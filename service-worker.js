c29df91d6b11d4e276632dc8c33d316bfebfb84c
bsommardahl <byron@sommardahl.com>
2020-05-09 04:50:45 +0000

    Deploying to gh-pages from  @ b6a4d9c6d6b055f410b51013600acca873847c0e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb925e5e8a2c52117740a89366889fb6.js"
+  "/precache-manifest.1530452bf0e2dd3801a6cf2d2af263d5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "codex-academy-website"});
