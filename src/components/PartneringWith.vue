9cd864cfd500ca23b85e098adc96c3fbf99baa06
Byron Sommardahl <byron@acklenavenue.com>
2021-07-06 12:55:44 -0700

    Adding scf  page.

--- src/components/PartneringWith.vue
+++ src/components/PartneringWith.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="partner" v-if="logo">
     <p>In partnership with</p>
-    <div><a :href="url" target="_blank">
-      <img class="partner" :src="logo" />
-    </a>
+    <div>
+      <a :href="url" target="_blank">
+        <img class="partner" :src="logo" />
+      </a>
     </div>
   </div>
 </template>
@@ -11,14 +12,27 @@
 <script>
 const partners = {
   pathrise: { logo: "img/partners/pathrise.svg", url: "https://pathrise.com" },
-  nashvillestate: { logo: "img/partners/nscc.png", url: "https://www.nscc.edu" },
+  nashvillestate: {
+    logo: "img/partners/nscc.png",
+    url: "https://www.nscc.edu",
+  },
+  scf: { logo: "img/partners/scf.jpg", url: "https://www.scf.edu/" },
   slttech: { logo: "img/partners/slt.jpg", url: "https://www.slttechla.com/" },
-  batonrougecc: { logo: "img/partners/brcc.jpg", url: "https://www.mybrcc.edu/" },
+  batonrougecc: {
+    logo: "img/partners/brcc.jpg",
+    url: "https://www.mybrcc.edu/",
+  },
   talently: { logo: "img/partners/talently.svg", url: "https://talently.tech" },
   flexiple: { logo: "img/partners/flexiple.png", url: "https://flexiple.com/" },
   outco: { logo: "img/partners/outco.png", url: "https://outco.io/" },
-  recruiterly: { logo: "img/partners/recruiterly.svg", url: "https://recruiterly.com/" },
-  bairesdev: { logo: "img/partners/bairesdev.svg", url: "https://bairesdev.com/" },
+  recruiterly: {
+    logo: "img/partners/recruiterly.svg",
+    url: "https://recruiterly.com/",
+  },
+  bairesdev: {
+    logo: "img/partners/bairesdev.svg",
+    url: "https://bairesdev.com/",
+  },
   arcdev: { logo: "img/partners/arcdev.svg", url: "https://arc.dev" },
   lifeway: { logo: "img/partners/lifeway.svg", url: "https://lifeway.com" },
   dice: { logo: "img/partners/dice.svg", url: "https://dice.com" },
---
+++ src/components/sections/corp/BootcampFeatures.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="row">
+    <div
+      class="mt-5 col-6 col-md-3 text-center"
+      v-for="f in features"
+      :key="f.name"
+    >
+      <Icon :name="f.icon" color="black" />
+      <p class="mt-4 muted">{{ f.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/Icon";
+
+export default {
+  components: { Icon },
+  data: () => ({
+    features: [
+      {
+        name: "Student Orientation",
+        bullets: [
+          "Adaptation of core orientation class to your bootcamp as needed.",
+        ],
+        icon: "branch",
+      },
+      {
+        name: "Student Progress Tracking",
+        bullets: [
+          "Frequent pace/trajectory re-calculation",
+          "Student low-progress alerts",
+          "Check-in meeting facilitation",
+        ],
+        icon: "database",
+      },
+
+      {
+        name: "Reporting",
+        bullets: [
+          "Student records available to host upon request",
+          "Frequent student progress reports",
+        ],
+        icon: "tdd",
+      },
+
+      {
+        name: "Student Success Management",
+        bullets: [
+          "Mentor introductions",
+          "Kicking off each level with a strategy meeting",
+          "Working with students to maintain goal-focused pace",
+        ],
+        icon: "okay",
+      },
+
+      {
+        name: "Community Support",
+        bullets: [
+          "Active developer/mentor/learner chat community",
+          "Daily monitoring and moderating",
+        ],
+        icon: "mentor",
+      },
+
+      {
+        name: "Payment and Financial Aid Services",
+        bullets: [
+          "Credit/Debit card payments (no transaction fees)",
+          "Student loans available",
+          "ISA’s available",
+        ],
+        icon: "money",
+      },
+
+      {
+        name: "Job Placement Services",
+        bullets: [
+          "Resume/CV feedback",
+          "Extensive and growing curated list of internship and hiring",
+          "partners",
+          "Job Application counseling/advice",
+          "Professional introductions",
+        ],
+        icon: "happy",
+      },
+
+      {
+        name: "Curriculum Support",
+        bullets: [
+          "Students are welcome to use CodeX Academy’s core full-stack",
+          "software development curriculum if desired.",
+          "If you need help adapting your own curriculum or learning",
+          "resources, we will provide assistance as needed.",
+        ],
+        icon: "design",
+      },
+    ],
+  }),
+};
+</script>
+
+<style></style>
