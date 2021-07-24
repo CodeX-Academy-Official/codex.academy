576b589d720552a96def9b3cdaa23d5b90e962b7
Byron Sommardahl <byron@acklenavenue.com>
2021-07-24 12:47:10 -0700

    Using puppeteer.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  testMatch: ["**/?(*.)(spec|test).[jt]s?(x)"]
-}
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["**/?(*.)(spec|test).[jt]s?(x)"],
+};
