d3ab2d28c95567082737f516059ffd5185344c86
Byron Sommardahl <byron@sommardahl.com>
2022-01-17 06:59:40 -0800

    Fixing pathway link.

--- src/store/pathways.ts
+++ src/store/pathways.ts
@@ -6,7 +6,7 @@ export const pathways = [
       "Able to build and deploy simple public websites using HTML and CSS.",
     badgeImageUrl: "img/badges/level1.png",
     pathwayUrl:
-      "https://badgr.com/public/pathway/5df3b7bc46e0fb002140cd03/element/5df3b7bc46e0fb002140cd02"
+      "https://codexacademy.badgr.com/public/pathway/5df3b7bc46e0fb002140cd03/element/5df3b7bc46e0fb002140cd02"
   },
   {
     name: "Front-End Developer  2",
@@ -15,7 +15,7 @@ export const pathways = [
       "Able to build and auto-deploy dynamic web applications using HTML, CSS and Javascript.",
     badgeImageUrl: "img/badges/level2.png",
     pathwayUrl:
-      "https://badgr.com/public/pathway/5df50e9d46e0fb002140d64f/element/5df50e9d46e0fb002140d64e"
+      "https://codexacademy.badgr.com/public/pathway/5df50e9d46e0fb002140d64f/element/5df50e9d46e0fb002140d64e"
   },
   {
     name: "Front-End Developer  3",
@@ -24,7 +24,7 @@ export const pathways = [
       "Able to build and auto-deploy responsive data-driven web applications using advanced HTML, CSS and Javascript.",
     badgeImageUrl: "img/badges/level3.png",
     pathwayUrl:
-      "https://badgr.com/public/pathway/5e0f533246e0fb002739c4f1/element/5e0f533246e0fb002739c4f0"
+      "https://codexacademy.badgr.com/public/pathway/5e0f533246e0fb002739c4f1/element/5e0f533246e0fb002739c4f0"
   },
   {
     name: "Full-Stack Developer 4",
@@ -33,7 +33,7 @@ export const pathways = [
       "Able to build backend-driven web applications using HTML, CSS, Javascript and NodeJS.",
     badgeImageUrl: "img/badges/level4.png",
     pathwayUrl:
-      "https://badgr.com/public/pathway/5e0f7a9d46e0fb00221bc2b9/element/5e0f7a9d46e0fb00221bc2b8"
+      "https://codexacademy.badgr.com/public/pathway/5e0f7a9d46e0fb00221bc2b9/element/5e0f7a9d46e0fb00221bc2b8"
   },
   {
     name: "Full-Stack Developer 5",
@@ -41,7 +41,7 @@ export const pathways = [
     description:
       "Able to build and deploy database-driven web applications using ReactJS and NodeJS.",
     badgeImageUrl: "img/badges/level5.png",
-    pathwayUrl: "https://badgr.com/public/pathway/5e0fb6a346e0fb002739c85b"
+    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fb6a346e0fb002739c85b"
   },
   {
     name: "Full-Stack Developer 6",
@@ -49,7 +49,7 @@ export const pathways = [
     description:
       "",
     badgeImageUrl: "img/badges/level6.png",
-    pathwayUrl: "https://badgr.com/public/pathway/5e0fb76446e0fb00221bc56b"
+    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fb76446e0fb00221bc56b"
   },
   {
     name: "Full-Stack Engineer 7",
@@ -57,7 +57,7 @@ export const pathways = [
     description:
       "",
     badgeImageUrl: "img/badges/level7.png",
-    pathwayUrl: "https://badgr.com/public/pathway/5f18a8fb4e528560cd3d7155"
+    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fb93446e0fb002739c864"
   },
   {
     name: "Full-Stack Engineer 8",
@@ -65,7 +65,7 @@ export const pathways = [
     description:
       "",
     badgeImageUrl: "img/badges/level8.png",
-    pathwayUrl: "https://badgr.com/public/pathway/5e0fc58846e0fb002739c897"
+    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fc58846e0fb002739c897"
   },
   {
     name: "Full-Stack Engineer 9",
@@ -73,6 +73,6 @@ export const pathways = [
     description:
       "",
     badgeImageUrl: "img/badges/level9.png",
-    pathwayUrl: "https://badgr.com/public/pathway/5f18a8fb4e528560cd3d7155"
+    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5f18a8fb4e528560cd3d7155"
   }
 ];
