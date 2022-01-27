6fbfff2f9b7e49614f8c0bc84c552ee049cf7a58
Byron Sommardahl <byron@sommardahl.com>
2022-01-27 11:59:44 -0600

    CICD course start

---
+++ src/store/temp.js
@@ -0,0 +1,27 @@
+var raw = {
+    "name": "Custom Field",
+    "dataType": "TEXT",
+    "placeholder": "Placeholder Text",
+    "options": [
+        "first option",
+        "last option"
+    ],
+    "acceptedFormat": [
+        ".pdf",
+        ".docx",
+        ".jpeg"
+    ],
+    "isMultipalFile": false,
+    "maxNumberOfFiles": 2,
+    "textBoxListOptions": [
+        {
+            "label": "First",
+            "prefillValue": ""
+        },
+        {
+            "label": "First",
+            "prefillValue": ""
+        }
+    ],
+    "position": 0
+};
