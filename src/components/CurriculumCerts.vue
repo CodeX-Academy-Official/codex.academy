649c9e765c89eac198ed1f1410ae6765d7fea1de
Jrmart12 <jr.martinez@codex.academy>
2022-04-08 15:15:34 -0600

    removed badges icon for pathways 3,6,9 for gold bg

--- src/components/CurriculumCerts.vue
+++ src/components/CurriculumCerts.vue
@@ -1,50 +1,49 @@
 <template>
   <div>
-      <div
-        class="row bg-white mb-5 border p-4"
-        v-for="cert in getCertifications"
-        :key="cert.id"
-      >
-        <div class="col-12 col-lg-3">
+    <div
+      class="row bg-white mb-5 border p-4"
+      v-for="cert in getCertifications"
+      :key="cert.id"
+    >
+      <div class="col-12 col-lg-3">
+        <div class="text-center">
           <div class="text-center">
-            <div class="text-center">
-              <h4 class="">{{ cert.name }}</h4>
-              <p class="">{{ cert.description }}</p>
-              <a :href="cert.learnMoreUrl" class="btn btn-primary">Learn More</a>
-
-            </div>
-          </div>
-        </div>
-        <div
-          class="col-12 col-md-4 col-lg-2"
-          v-for="pathway in levels(cert.id)"
-          :key="pathway.name"
-        >
-          <div class="text-center">
-            <h6 class="mb-3">
-              {{ pathway.name }}
-            </h6>
-            <a :href="pathway.pathwayUrl" target="_blank">
-              <img
-                class=""
-                :src="pathway.badgeImageUrl"
-                :alt="pathway.name"
-                style="width: 70%"
-              />
-            </a>
-          </div>
-        </div>
-        <div class="col-12 col-lg-3 mt-3">
-          <div class="text-center">
-            <img
-              class="card-img-top"
-              :src="cert.badgeUrl"
-              :alt="cert.name"
-              style="width: 70%"
-            />
+            <h4 class="">{{ cert.name }}</h4>
+            <p class="">{{ cert.description }}</p>
+            <a :href="cert.learnMoreUrl" class="btn btn-primary">Learn More</a>
           </div>
         </div>
       </div>
+      <div
+        class="col-12 col-md-4 col-lg-3"
+        v-for="pathway in levels(cert.id)"
+        :key="pathway.name"
+      >
+        <div class="text-center">
+          <h6 class="mb-3">
+            {{ pathway.name }}
+          </h6>
+          <a :href="pathway.pathwayUrl" target="_blank">
+            <img
+              class=""
+              :src="pathway.badgeImageUrl"
+              :alt="pathway.name"
+              style="width: 70%"
+            />
+          </a>
+        </div>
+      </div>
+      <!-- <div class="col-12 col-lg-3 mt-3">
+        <div class="text-center">
+          <img
+            class="card-img-top"
+            :src="cert.badgeUrl"
+            :alt="cert.name"
+            style="width: 70%"
+          />
+        </div>
+      </div> -->
+    </div>
   </div>
 </template>
 
@@ -63,6 +62,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/store/pathways.ts
+++ src/store/pathways.ts
@@ -6,7 +6,7 @@ export const pathways = [
       "Able to build and deploy simple public websites using HTML and CSS.",
     badgeImageUrl: "img/badges/level1.png",
     pathwayUrl:
-      "https://codexacademy.badgr.com/public/pathway/5df3b7bc46e0fb002140cd03/element/5df3b7bc46e0fb002140cd02"
+      "https://codexacademy.badgr.com/public/pathway/5df3b7bc46e0fb002140cd03/element/5df3b7bc46e0fb002140cd02",
   },
   {
     name: "Front-End Developer  2",
@@ -15,16 +15,16 @@ export const pathways = [
       "Able to build and auto-deploy dynamic web applications using HTML, CSS and Javascript.",
     badgeImageUrl: "img/badges/level2.png",
     pathwayUrl:
-      "https://codexacademy.badgr.com/public/pathway/5df50e9d46e0fb002140d64f/element/5df50e9d46e0fb002140d64e"
+      "https://codexacademy.badgr.com/public/pathway/5df50e9d46e0fb002140d64f/element/5df50e9d46e0fb002140d64e",
   },
   {
     name: "Front-End Developer  3",
     cert: "fed",
     description:
-      "Able to build and auto-deploy responsive data-driven web applications using advanced HTML, CSS and Javascript.",
-    badgeImageUrl: "img/badges/level3.png",
+      "Develop and deploy complex, dynamic front-end web applications using HTML, CSS, JavaScript, React and Firebase.",
+    badgeImageUrl: "img/badges/fed-master.png",
     pathwayUrl:
-      "https://codexacademy.badgr.com/public/pathway/5e0f533246e0fb002739c4f1/element/5e0f533246e0fb002739c4f0"
+      "https://codexacademy.badgr.com/public/pathway/5e0f533246e0fb002739c4f1/element/5e0f533246e0fb002739c4f0",
   },
   {
     name: "Full-Stack Developer 4",
@@ -33,7 +33,7 @@ export const pathways = [
       "Able to build backend-driven web applications using HTML, CSS, Javascript and NodeJS.",
     badgeImageUrl: "img/badges/level4.png",
     pathwayUrl:
-      "https://codexacademy.badgr.com/public/pathway/5e0f7a9d46e0fb00221bc2b9/element/5e0f7a9d46e0fb00221bc2b8"
+      "https://codexacademy.badgr.com/public/pathway/5e0f7a9d46e0fb00221bc2b9/element/5e0f7a9d46e0fb00221bc2b8",
   },
   {
     name: "Full-Stack Developer 5",
@@ -41,38 +41,41 @@ export const pathways = [
     description:
       "Able to build and deploy database-driven web applications using ReactJS and NodeJS.",
     badgeImageUrl: "img/badges/level5.png",
-    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fb6a346e0fb002739c85b"
+    pathwayUrl:
+      "https://codexacademy.badgr.com/public/pathway/5e0fb6a346e0fb002739c85b",
   },
   {
     name: "Full-Stack Developer 6",
     cert: "fsd",
     description:
-      "",
-    badgeImageUrl: "img/badges/level6.png",
-    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fb76446e0fb00221bc56b"
+      "Develop challenging full-stack, data-driven web applications using best practices in technologies like React, NodeJS, TypeScript and C Sharp",
+    badgeImageUrl: "img/badges/fsd-master.png",
+    pathwayUrl:
+      "https://codexacademy.badgr.com/public/pathway/5e0fb76446e0fb00221bc56b",
   },
   {
     name: "Full-Stack Engineer 7",
     cert: "fse",
-    description:
-      "",
+    description: "",
     badgeImageUrl: "img/badges/level7.png",
-    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fb93446e0fb002739c864"
+    pathwayUrl:
+      "https://codexacademy.badgr.com/public/pathway/5e0fb93446e0fb002739c864",
   },
   {
     name: "Full-Stack Engineer 8",
     cert: "fse",
-    description:
-      "",
+    description: "",
     badgeImageUrl: "img/badges/level8.png",
-    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5e0fc58846e0fb002739c897"
+    pathwayUrl:
+      "https://codexacademy.badgr.com/public/pathway/5e0fc58846e0fb002739c897",
   },
   {
     name: "Full-Stack Engineer 9",
     cert: "fse",
     description:
-      "",
-    badgeImageUrl: "img/badges/level9.png",
-    pathwayUrl: "https://codexacademy.badgr.com/public/pathway/5f18a8fb4e528560cd3d7155"
-  }
+      "Lead Agile teams to develop complex, secure and well-built full-stack web applications using multiple modern front-end and back-end technologies.",
+    badgeImageUrl: "img/badges/fse-master.png",
+    pathwayUrl:
+      "https://codexacademy.badgr.com/public/pathway/5f18a8fb4e528560cd3d7155",
+  },
 ];
